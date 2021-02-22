a77cca7fdcf3dfe7f43916b10a1ced53c7e1161e
cgiguere <c.giguere42@gmail.com>
2021-02-22 17:51:50 -0500

    basic write objects

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,93 @@
+use std::{
+    fs::{self, File},
+    io::Write,
+    iter::repeat_with,
+    path::PathBuf,
+};
 
+use anyhow::Result;
+use crypto::{digest::Digest, sha1::Sha1};
+use flate2::{write::ZlibEncoder, Compression};
+use fs::metadata;
+
+pub enum Object {
+    Blob(Vec<u8>),
+}
+
+impl Object {
+    pub fn serialize_type(&self) -> &[u8] {
+        match self {
+            Object::Blob(_) => b"blob",
+        }
+    }
+
+    pub fn serialize_data(&self) -> &[u8] {
+        match self {
+            Object::Blob(data) => &data,
+        }
+    }
+
+    pub fn serialize(&self) -> Vec<u8> {
+        let data = self.serialize_data();
+        let mut content = vec![];
+        content.append(&mut self.serialize_type().to_vec());
+        content.push(b' ');
+        content.append(&mut data.len().to_ne_bytes().to_vec());
+        content.push(b'\x00');
+        content.append(&mut data.to_vec());
+
+        content
+    }
+}
+
+pub struct Database {
+    pub path: PathBuf,
+}
+
+impl Database {
+    pub fn store(&self, obj: Object) -> Result<()> {
+        let content = obj.serialize();
+        let mut hasher = Sha1::new();
+        hasher.input(&content);
+        let hash = hasher.result_str();
+
+        write(&self.path, hash, content).expect("Failed to write object");
+
+        Ok(())
+    }
+}
+
+fn write(path: &PathBuf, hash: String, content: Vec<u8>) -> Result<()> {
+    let object_path = path.join(&hash[..=1]).join(&hash[2..]);
+    let temp_path = object_path.parent().unwrap().join(generate_temp_name());
+
+    let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
+    encoder
+        .write_all(&content)
+        .expect("Failed to write to encoder");
+    let compressed = &encoder.finish().expect("Failed to compress content");
+
+    fs::create_dir_all(&object_path.parent().unwrap()).expect(&format!(
+        "Failed to create object_path at: {}",
+        object_path.display()
+    ));
+    let mut file = File::create(&temp_path).expect(&format!(
+        "Failed to create temp file at: {}",
+        temp_path.display()
+    ));
+    file.write_all(compressed)
+        .expect("Failed to write to temp file");
+
+    let mut perms = fs::metadata(&temp_path)?.permissions();
+    perms.set_readonly(false);
+    fs::set_permissions(&temp_path, perms)?;
+
+    fs::rename(temp_path, object_path).expect("Failed to rename temp file");
+
+    Ok(())
+}
+
+fn generate_temp_name() -> String {
+    let s: String = repeat_with(fastrand::alphanumeric).take(6).collect();
+    format!("tmp_obj_#{}", s)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-use std::{env, io::Write, path::PathBuf};
+use std::{
+    fs::{self, DirEntry},
+    io,
+    path::{Path, PathBuf},
+};
 
 use anyhow::Result;
 use clap::Clap;
+
+const GIT_FOLDER: &str = "git"; // TODO reset to .git
 
 /// The stupid content tracker
 #[derive(Clap)]
@@ -14,6 +20,7 @@ struct Opts {
 #[derive(Clap)]
 enum Commands {
     Init(Init),
+    Commit,
 }
 
 #[derive(Clap)]
@@ -28,13 +35,52 @@ fn main() -> Result<()> {
 
     match opts.commands {
         Commands::Init(args) => {
-            let git_path = args.path.unwrap_or(std::env::current_dir()?).join("git"); // TODO reset to .git
-            std::fs::create_dir_all(git_path.join("objects"))?;
-            std::fs::create_dir_all(git_path.join("refs"))?;
-            println!(
-                "Initialized git_rs repository in {}",
-                git_path.to_string_lossy()
-            );
+            let git_path = args
+                .path
+                .unwrap_or(std::env::current_dir()?)
+                .join(GIT_FOLDER);
+            fs::create_dir_all(git_path.join("objects"))?;
+            fs::create_dir_all(git_path.join("refs"))?;
+            println!("Initialized git_rs repository in {}", git_path.display());
+        }
+        Commands::Commit => {
+            let root_path = std::env::current_dir()?;
+            let git_path = root_path.join(GIT_FOLDER);
+            let objects_path = git_path.join("objects");
+            let db = git_rs::Database { path: objects_path };
+
+            visit_dirs(root_path.as_path(), &|file| {
+                let file_path = &file.path();
+                let data =
+                    fs::read(file_path).expect(&format!("Failed to read {}", file_path.display()));
+                let blob = git_rs::Object::Blob(data);
+
+                db.store(blob)?;
+
+                Ok(())
+            })?;
+        }
+    }
+
+    Ok(())
+}
+
+/// Visits all the files in a directory recursively
+fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry) -> Result<()>) -> Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            // TODO implement .gitignore
+            match path.file_name().unwrap().to_str().unwrap() {
+                ".git" | GIT_FOLDER | "target" => continue,
+                _ => (),
+            }
+            if path.is_dir() {
+                visit_dirs(&path, cb)?;
+            } else {
+                cb(&entry)?;
+            }
         }
     }
 
