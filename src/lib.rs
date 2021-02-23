076c1175f819b042499bc00a5262f10da5fc9ff0
cgiguere <c.giguere42@gmail.com>
2021-02-22 21:51:00 -0500

    treeish

--- src/lib.rs
+++ src/lib.rs
@@ -2,39 +2,55 @@ use std::{
     fs::{self, File},
     io::Write,
     iter::repeat_with,
-    path::PathBuf,
+    path::{Path, PathBuf},
 };
 
 use anyhow::Result;
 use crypto::{digest::Digest, sha1::Sha1};
 use flate2::{write::ZlibEncoder, Compression};
-use fs::metadata;
+
+const MODE: &str = "100644";
 
 pub enum Object {
     Blob(Vec<u8>),
+    Tree(Vec<Entry>),
 }
 
 impl Object {
     pub fn serialize_type(&self) -> &[u8] {
         match self {
             Object::Blob(_) => b"blob",
+            Object::Tree(_) => b"tree",
         }
     }
 
-    pub fn serialize_data(&self) -> &[u8] {
+    pub fn serialize_data(&self) -> Vec<u8> {
         match self {
-            Object::Blob(data) => &data,
+            Object::Blob(data) => data.clone(),
+            Object::Tree(entries) => {
+                let mut entries = entries.to_vec();
+                entries.sort_by(|a, b| a.name.cmp(&b.name));
+                let mut tree = vec![];
+                for entry in entries.iter() {
+                    let mut entry_vec: Vec<u8> = format!("{} {}\0", MODE, entry.name.display())
+                        .as_bytes()
+                        .to_vec();
+                    entry_vec.extend_from_slice(&entry.object_id.as_bytes()[..2]);
+                    tree.extend_from_slice(&entry_vec);
+                }
+                tree
+            }
         }
     }
 
     pub fn serialize(&self) -> Vec<u8> {
         let data = self.serialize_data();
         let mut content = vec![];
-        content.append(&mut self.serialize_type().to_vec());
+        content.extend_from_slice(self.serialize_type());
         content.push(b' ');
-        content.append(&mut data.len().to_ne_bytes().to_vec());
-        content.push(b'\x00');
-        content.append(&mut data.to_vec());
+        content.extend_from_slice(&data.len().to_ne_bytes());
+        content.push(b'\0');
+        content.extend_from_slice(&data);
 
         content
     }
@@ -45,49 +61,46 @@ pub struct Database {
 }
 
 impl Database {
-    pub fn store(&self, obj: Object) -> Result<()> {
-        let content = obj.serialize();
-        let mut hasher = Sha1::new();
-        hasher.input(&content);
-        let hash = hasher.result_str();
-
-        write(&self.path, hash, content).expect("Failed to write object");
-
-        Ok(())
+    pub fn store(&self, obj: Object) -> Result<String> {
+        let object_id = hash(&obj);
+        write(&self.path, &object_id, obj.serialize()).expect("Failed to write object to database");
+        Ok(object_id)
     }
 }
 
-fn write(path: &PathBuf, hash: String, content: Vec<u8>) -> Result<()> {
-    let object_path = path.join(&hash[..=1]).join(&hash[2..]);
+fn hash(obj: &Object) -> String {
+    let mut hasher = Sha1::new();
+    hasher.input(&obj.serialize());
+    hasher.result_str()
+}
+
+fn write(path: &Path, object_id: &str, content: Vec<u8>) -> Result<()> {
+    let object_path = path.join(&object_id[..2]).join(&object_id[2..]);
     let temp_path = object_path.parent().unwrap().join(generate_temp_name());
 
-    let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
-    encoder
-        .write_all(&content)
-        .expect("Failed to write to encoder");
-    let compressed = &encoder.finish().expect("Failed to compress content");
+    let mut encoder = ZlibEncoder::new(Vec::new(), Compression::fast());
+    encoder.write_all(&content)?;
+    let compressed = &encoder.finish()?;
 
-    fs::create_dir_all(&object_path.parent().unwrap()).expect(&format!(
-        "Failed to create object_path at: {}",
-        object_path.display()
-    ));
-    let mut file = File::create(&temp_path).expect(&format!(
-        "Failed to create temp file at: {}",
-        temp_path.display()
-    ));
-    file.write_all(compressed)
-        .expect("Failed to write to temp file");
+    fs::create_dir_all(&object_path.parent().unwrap())?;
+    let mut file = File::create(&temp_path)?;
+    file.write_all(compressed)?;
 
-    let mut perms = fs::metadata(&temp_path)?.permissions();
-    perms.set_readonly(false);
-    fs::set_permissions(&temp_path, perms)?;
+    fs::rename(temp_path, &object_path)?;
 
-    fs::rename(temp_path, object_path).expect("Failed to rename temp file");
+    println!("writing {} to {}", path.display(), object_path.display());
 
     Ok(())
 }
 
+/// Generates a random string of 6 alphanumerical characters
 fn generate_temp_name() -> String {
     let s: String = repeat_with(fastrand::alphanumeric).take(6).collect();
     format!("tmp_obj_#{}", s)
+}
+
+#[derive(Clone)]
+pub struct Entry {
+    pub name: PathBuf,
+    pub object_id: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use std::{
-    fs::{self, DirEntry},
-    io,
-    path::{Path, PathBuf},
-};
+use std::{fs, io::Read, path::PathBuf};
 
 use anyhow::Result;
 use clap::Clap;
+use git_rs::Entry;
+use glob::glob;
+
+use flate2::read::{GzDecoder, ZlibDecoder};
+use flate2::write::ZlibEncoder;
+use flate2::Compression;
+use std::io::prelude::*;
 
 const GIT_FOLDER: &str = "git"; // TODO reset to .git
 
@@ -21,6 +24,8 @@ struct Opts {
 enum Commands {
     Init(Init),
     Commit,
+    Read(ReadArgs),
+    Clear,
 }
 
 #[derive(Clap)]
@@ -28,6 +33,13 @@ struct Init {
     /// Where to create the repository.
     #[clap(name = "directory", parse(from_os_str))]
     path: Option<PathBuf>,
+}
+
+#[derive(Clap)]
+struct ReadArgs {
+    /// Where to create the repository.
+    #[clap(name = "file", parse(from_os_str))]
+    path: PathBuf,
 }
 
 fn main() -> Result<()> {
@@ -49,40 +61,75 @@ fn main() -> Result<()> {
             let objects_path = git_path.join("objects");
             let db = git_rs::Database { path: objects_path };
 
-            visit_dirs(root_path.as_path(), &|file| {
-                let file_path = &file.path();
-                let data =
-                    fs::read(file_path).expect(&format!("Failed to read {}", file_path.display()));
-                let blob = git_rs::Object::Blob(data);
+            let entries: Vec<Entry> = glob("*")
+                .expect("Failed to read glob pattern")
+                .filter_map(|path| match path {
+                    Ok(path) => {
+                        if path.is_dir() {
+                            None
+                        } else {
+                            match path.file_name().unwrap().to_str().unwrap() {
+                                ".git" | GIT_FOLDER | "target" => None,
+                                _ => Some(path),
+                            }
+                        }
+                    }
+                    _ => None,
+                })
+                .map(|path| {
+                    let data = fs::read(&path)
+                        .unwrap_or_else(|_| panic!("Failed to read {}", &path.display()));
+                    let blob = git_rs::Object::Blob(data);
+                    let object_id = db.store(blob).expect("Failed to store object in database");
+                    Entry {
+                        name: path,
+                        object_id,
+                    }
+                })
+                .collect();
 
-                db.store(blob)?;
+            let tree = git_rs::Object::Tree(entries);
+            let object_id = db.store(tree)?;
 
-                Ok(())
-            })?;
+            println!("tree: {}", object_id);
+        }
+        Commands::Read(args) => {
+            let compressed_file = fs::read(args.path)?;
+            let mut d = ZlibDecoder::new(&compressed_file[..]);
+            let mut s = String::new();
+            d.read_to_string(&mut s).expect("Failed to read to string");
+            println!("{}", s);
+        }
+        Commands::Clear => {
+            std::fs::remove_dir_all(GIT_FOLDER)?;
         }
     }
 
     Ok(())
 }
 
-/// Visits all the files in a directory recursively
-fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry) -> Result<()>) -> Result<()> {
-    if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            // TODO implement .gitignore
-            match path.file_name().unwrap().to_str().unwrap() {
-                ".git" | GIT_FOLDER | "target" => continue,
-                _ => (),
-            }
-            if path.is_dir() {
-                visit_dirs(&path, cb)?;
-            } else {
-                cb(&entry)?;
-            }
-        }
-    }
+#[cfg(test)]
+mod tests {
+    use flate2::{read::ZlibDecoder, write::ZlibEncoder, Compression};
+    use std::io::prelude::*;
 
-    Ok(())
+    #[test]
+    fn flate() -> anyhow::Result<()> {
+        let mut encoder = ZlibEncoder::new(Vec::new(), Compression::fast());
+        encoder.write_all(b"Hello world")?;
+        let compressed = encoder.finish()?;
+
+        std::fs::create_dir_all("./tmp")?;
+        let mut file = std::fs::File::create("./tmp/compressed")?;
+        file.write_all(&compressed)?;
+
+        let compressed_file = std::fs::read("./tmp/compressed")?;
+        let mut decoder = ZlibDecoder::new(&compressed_file[..]);
+        let mut s = String::new();
+        decoder.read_to_string(&mut s).unwrap();
+
+        std::fs::remove_dir_all("./tmp")?;
+
+        Ok(())
+    }
 }
